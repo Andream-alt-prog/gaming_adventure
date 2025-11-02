d9a0ce6b6f167e4f6b87338b218c8631b4713074
Andream-alt-prog <usachov.andrew@gmail.com>
2025-11-02 11:33:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function gotosite1(){
+    const Game1="https://www.roblox.com/download";
+    window.location.href=Game1;
+}
+function gotosite2(){
+    const Game2="https://brawl-stars.ru.download.it/";
+    window.location.href=Game2;
+}
+function gotosite3(){
+    const Game3="https://www.bluestacks.com/ru/apps/action/free-fire-battlegrounds-on-pc.html";
+    window.location.href=Game3;
+}
+function gotosite4(){
+    const Game4="https://www.bluestacks.com/ru/apps/action/free-fire-battlegrounds-on-pc.html";
+    window.location.href=Game4;
+}
+    function gotosite5(){
+    const Game5="https://www.minecraft.net/ru-ru";
+    window.location.href=Game5;
+}
+   function gotosite6(){
+    const Game6="https://geometry-game.io/";
+    window.location.href=Game6;
+}
+ function gotosite7(){
+    const Game7="https://www.fortnite.com/?lang=ru";
+    window.location.href=Game7;
+}
+function gotosite8(){
+    const Game8="https://play.google.com/store/apps/details?id=com.supercell.clashofclans&hl=ru";
+    window.location.href=Game8;
+}
+function gotosite9(){
+    const Game9="https://store.steampowered.com/app/391540/Undertale/?l=ukrainian";
+    window.location.href=Game9;
+}
+function gotosite10(){
+    const Game10="https://play.google.com/store/apps/details?id=com.tencent.ig&hl=uk";
+    window.location.href=Game10;
+}
+
+
+
+
